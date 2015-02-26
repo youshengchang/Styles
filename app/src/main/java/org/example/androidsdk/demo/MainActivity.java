e64ba23f8020a48d89ad38d9f8e21361beeadef8
youshengchang <changyousheng@yahoo.com>
2015-02-25 21:14:52 -0800

    Initial commit

---
+++ app/src/main/java/org/example/androidsdk/demo/MainActivity.java
@@ -0,0 +1,29 @@
+package org.example.androidsdk.demo;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		View layout = findViewById(R.id.layout);
+		Drawable background = layout.getBackground();
+		background.setAlpha(120);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+}
